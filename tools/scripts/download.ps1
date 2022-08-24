f27cb85e55fd00555fba7d8a4bd32dc68889f4e3
Enner Pérez <ennerperez@gmail.com>
2022-08-23 19:00:30 -0500

    Download Script Fix

--- tools/scripts/download.ps1
+++ tools/scripts/download.ps1
@@ -13,9 +13,14 @@ else
     .\_variables.ps1
 }
 
+# Files
+$bzip2_url = "https://github.com/philr/bzip2-windows/releases/download/v1.0.8.0/bzip2-1.0.8.0-win-x64.zip"
+$steamcmd_url = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+$depotdownloader_url = "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.4.7/depotdownloader-2.4.7.zip"
+
 if (!(Test-Path -path "../bzip2.zip"))
 {
-    Invoke-WebRequest https://github.com/philr/bzip2-windows/releases/download/v1.0.8.0/bzip2-1.0.8.0-win-x64.zip -O ../bzip2.zip
+    Invoke-WebRequest $bzip2_url -O ../bzip2.zip
     Expand-Archive ../bzip2.zip ../bzip2 -Force
 }
 
@@ -23,7 +28,7 @@ if ($mode -eq "steamcmd")
 {
     if (!(Test-Path -path "../steamcmd.zip"))
     {
-        Invoke-WebRequest https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip -O ../steamcmd.zip
+        Invoke-WebRequest $steamcmd_url -O ../steamcmd.zip
         Expand-Archive ../steamcmd.zip ../steamcmd -Force
     }
     Invoke-Expression "../steamcmd/steamcmd.exe +force_install_dir ../depots/$steam_app +login $steam_username $steam_password +app_update $steam_app validate +quit"
@@ -32,7 +37,7 @@ if ($mode -eq "depotdownloader")
 {
     if (!(Test-Path -path "../depotdownloader.zip"))
     {
-        Invoke-WebRequest https://github.com/SteamRE/DepotDownloader/releases/latest/download/depotdownloader-2.4.6.zip -O ../depotdownloader.zip
+        Invoke-WebRequest $depotdownloader_url -O ../depotdownloader.zip
         Expand-Archive ../depotdownloader.zip ../depotdownloader -Force
     }
     dotnet ../depotdownloader/DepotDownloader.dll -username $steam_username -password $steam_password -app $steam_app -dir ../depots/$steam_app
